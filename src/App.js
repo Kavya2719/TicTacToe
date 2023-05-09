f68ba523ac88b98564ad3e1a0272852ad0766b98
Kavya Gupta_323 <rkgqwe@gmail.com>
2023-05-09 04:11:10 +0000

    "Initial commit"

--- src/App.js
+++ src/App.js
@@ -145,10 +145,7 @@ export default function Game() {
     );
   });
 
-<<<<<<< HEAD
-
-=======
->>>>>>> new_branch
+  
   let congo = "";
   if(winner && winner !== undefined) congo = <h3 className="congoMessage"> Congratulations {winner} !! </h3>
 
